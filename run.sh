7144289e8d7954dc14c2b68ff9fd9dff90b3ff38
Warren <85319175+Warren-Codes@users.noreply.github.com>
2024-05-08 16:00:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,8 @@
-pipeline {
-    agent any
-    stages {
-        stage('Sleep') {
-            steps {
-                script {
-                    echo 'I am sleeping for a while'
-                    sleep(30) // Sleep for 30 seconds
-                }
-            }
-        }
-        stage('After Sleep') {
-            steps {
-                echo 'Already woke up!'
-            }
-        }
-    }
-}
+echo "Starting build..."
+
+for i in {1..30}; do
+    echo "Number: $i"
+    sleep 1  # Sleep for 1 second
+done
+
+echo "Build completed!"
