f12708d4b97a4e71dc80fa337041c9d418134c40
Warren-Codes <warren_bye@instance-jenkins-wb.europe-west1-d.c.lbg-mea-18.internal>
2024-05-09 09:56:17 +0000

    first commit

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-echo "Starting build after my sleep...in 30 seconds..."
-
-
-echo Hello from the Jenkins job named: ${JOB_NAME} 
-touch 1.txt 2.txt 3.txt 4.txt 5.txt zip archive.zip *.txt
+hello world
